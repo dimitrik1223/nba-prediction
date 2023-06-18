805d263583f1844b662e537f1c8e91be0857c759
Dimitri Kestenbaum <77963343+dimitrik1223@users.noreply.github.com>
2023-06-18 20:50:26 +0200

    reformat db utils

--- db/utils.py
+++ db/utils.py
@@ -4,21 +4,39 @@ import time
 from sqlalchemy import create_engine
 
 def create_conn(username, password, database):
-	db = create_engine(f"postgresql://{username}:{password}@localhost:5432/{database}")
-	return db
+	"""
+	Creates connection to Postgres database.
 
-def write_table(conn, dfs, table_names):
+	args:
+	username (str): Postgres username
+	password (str): Password for Postgres user
+	database (str): Postgres database to load
+
+	returns:
+	Postgres database connection
+	"""
+	postgres_conn = create_engine(f"postgresql://{username}:{password}@localhost:5432/{database}")
+	return postgres_conn
+
+def write_table(conn, stats_dfs, table_names):
+	"""
+	Writes tables to Postgres database if they don't already exist
+
+	args:
+	dfs (list): DataFrames to write to database
+	table_names (list): Names to use for tables loaded
+	"""
 	logging.basicConfig(level=logging.INFO)
 	try:
-		dfs = [dfs]
+		stats_dfs = [stats_dfs]
 		table_names = [table_names]
-	except:
-		raise Exception("DataFrames and data table names need to be passed as lists")
+	except TypeError as exception:
+		raise Exception("DataFrames and data table names need to be passed as lists") from exception
 
-	for index, df in enumerate(dfs):
+	for index, stats_df in enumerate(stats_dfs):
 		start = time.time()
 		table_name = table_names[index]
-		df.to_sql(table_name, conn, if_exists="replace",index=False)
+		stats_df.to_sql(table_name, conn, if_exists="replace",index=False)
 		end = time.time()
 		duration = round(end - start)
-		logging.info(f"Table {table_name} loaded in: {duration} seconds")
+		logging.info("Table %s loaded in: %s seconds", table_name, duration)
