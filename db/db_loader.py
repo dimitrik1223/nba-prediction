2ae592e9e437b5fafc25ec1b1c178d0649a38dfe
Dimitri Kestenbaum <77963343+dimitrik1223@users.noreply.github.com>
2023-06-19 12:52:16 +0200

    fix formatting

--- db/db_loader.py
+++ db/db_loader.py
@@ -1,13 +1,15 @@
-import os
-import logging
-
 from .utils import create_conn, write_table
 
-def load_db(dfs, table_names, postgres_config):
-	db = create_conn(**postgres_config)
-	conn = db.connect()
+def load_db(dfs, table_names, postgres_config) -> None:
+	"""
+	Writes DataFrames to Postgres database tables if they don't
+	already exist in the database.
+
+	args:
+	postgres_config (dict): Stores the username, password, and database credentials
+	for the Postgres database the tables should be loaded in.
+	"""
+	postgres_conn = create_conn(**postgres_config)
+	conn = postgres_conn.connect()
 	write_table(conn, dfs, table_names)
 	conn.close()
-
-
-
