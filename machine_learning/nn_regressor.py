730d8cb813cb5504838bcfc1a976f149b5d9ee59
Dimitri Kestenbaum <77963343+dimitrik1223@users.noreply.github.com>
2023-06-27 15:42:19 +0200

    add functionality to predict and display predictions

--- machine_learning/nn_regressor.py
+++ machine_learning/nn_regressor.py
@@ -1,6 +1,7 @@
 import copy
 import pickle
 import numpy as np
+import pandas as pd
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -59,26 +60,27 @@ class Net(nn.Module):
 		outputs = F.relu(self.fc2(outputs))
 		return self.fc3(outputs)
 
+def get_predictors(data):
+	"""
+	Return dataframe of MinMax scaled predictors.
+	"""
+	predictors = data[[
+		col for col in data.columns if data[col].dtype in ("float64", "int64")
+		and col not in ("Pts Won", "Pts Max", "Share")
+	]]
+	scaler = MinMaxScaler()
+	scaled_predictor_arr = scaler.fit_transform(predictors)
+	predictors = pd.DataFrame(scaled_predictor_arr)
+
+	return predictors
+
 def train_model(train_set):
 	"""
 	returns:
 	scaled_predictor_arr (array): Numpy array of scaled predictors
 	"""
-	predictors = train_set[[
-		col for col in train_set.columns if train_set[col].dtype in ("float64", "int64")
-		and col not in ("Pts Won", "Pts Max", "Share")
-	]]
-	device = (
-	"cuda"
-	if torch.cuda.is_available()
-	else "mps"
-	if torch.backends.mps.is_available()
-	else "cpu"
-	)
-	print(f"Using {device} device")
-	scaler = MinMaxScaler()
-	scaled_predictor_arr = scaler.fit_transform(predictors)
-	x_full = scaled_predictor_arr
+	scaled_predictors = get_predictors(train_set)
+	x_full = scaled_predictors.values
 	y_full = train_set["Pts Won"].values
 	x_train, x_test, y_train, y_test = train_test_split(
 	x_full, y_full, test_size=0.25, random_state=0
@@ -86,8 +88,8 @@ def train_model(train_set):
 	# Numpy to Torch Tensor
 	x_train = torch.Tensor(x_train).float()
 	y_train = torch.Tensor(y_train).float()
-	x_test = torch.Tensor(x_test).float().to(device)
-	y_test = torch.Tensor(y_test).float().to(device)
+	x_test = torch.Tensor(x_test).float().to("cpu")
+	y_test = torch.Tensor(y_test).float().to("cpu")
 	BATCH_SIZE = 16
 	dataset_train = TensorDataset(x_train, y_train)
 	dataloader_train = DataLoader(dataset_train, \
@@ -107,8 +109,8 @@ def train_model(train_set):
 		pbar = tqdm.tqdm(steps)
 		model.train()
 		for i, (x_batch, y_batch) in pbar:
-			y_batch_pred = model(x_batch.to(device)).flatten()
-			loss = loss_fn(y_batch_pred, y_batch.to(device))
+			y_batch_pred = model(x_batch.to("cpu")).flatten()
+			loss = loss_fn(y_batch_pred, y_batch.to("cpu"))
 			optimizer.zero_grad()
 			loss.backward()
 			optimizer.step()
@@ -126,15 +128,36 @@ def train_model(train_set):
 				pbar.set_description(f"Epoch: {epoch}, tloss {loss:}")
 	with open("flask_app/mvp_model.pkl", "wb") as file:
 		pickle.dump(model, file)
-	
-	return scaled_predictor_arr
+
+	return scaled_predictors
+
 
 def predict(model, data):
 	"""
 	Prediction method
 	"""
-	preds = model(data).detach().numpy()
+	tensor = torch.Tensor(data.values, device="cpu").type(torch.float32)
+	preds = model(tensor).detach().numpy()
 	# Negative preds to 0
 	mvp_preds = np.where(preds < 0, 0, preds)
-	
+
 	return mvp_preds
+
+def get_predicted_mvp(data, preds, year):
+	"""
+	Prints predicted and actual MVP for a given year.
+	"""
+	data["Predicted Pts Won"] = preds
+	year_stats = data[data["Year"] == year]
+	if not year_stats.empty:
+		max_pred_pts = year_stats["Predicted Pts Won"].max()
+		max_pts = year_stats["Pts Won"].max()
+		mvp_pred = year_stats.loc[year_stats["Predicted Pts Won"] == max_pred_pts, "Player"].iloc[0]
+		mvp = year_stats.loc[year_stats["Pts Won"] == max_pts, "Player"].iloc[0]
+		print(f"{year} - Predicted MVP: {mvp_pred}, Actual MVP: {mvp}")
+
+		return mvp_pred
+
+	year_start = data["Year"].min()
+	year_end = data["Year"].max()
+	raise Exception(f"Year must fall within the range from {year_start} to {year_end}")
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import os
+import pickle
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
-
+#pylint: disable=wrong-import-position
 from nba_stats_scraping.nba_stats_scraper import Nba_stats_scraper
 from nba_stats_scraping.stats_cleaning import clean_data, create_stats_basetable
-from machine_learning.nn_regressor import train_nn
+from machine_learning.nn_regressor import train_model, predict, get_predicted_mvp
 from db.db_loader import load_db
 
 
 def main(postgres_config, year_start=2022, year_end=2024) -> None:
+	"""
+	Scrape statistics, train model and get predictions.
+	"""
 	scraper = Nba_stats_scraper(year_start, year_end)
 	mvp_stats = scraper.scrape_mvp_stats()
 	per_game_stats = scraper.scrape_per_game_stats()
@@ -16,15 +20,17 @@ def main(postgres_config, year_start=2022, year_end=2024) -> None:
 		mvp_stats, per_game_stats, team_standings
 	)
 	all_stats = create_stats_basetable(mvp_stats, per_game_stats, team_standings)
-	load_db(dfs=all_stats, table_names="mvp_stats", postgres_config=postgres_config)
-	# train_nn(all_stats)
-	
+	predictors = train_model(train_set=all_stats)
+	load_db(dfs=predictors, table_names="mvp_predictors", postgres_config=postgres_config)
+	with open("flask_app/mvp_model.pkl", "rb") as file:
+		model = pickle.load(file)
+	preds = predict(model=model, data=predictors)
+	get_predicted_mvp(data=all_stats, preds=preds, year=2022)
 
 if __name__ == "__main__":
-	postgres_config = {
+	config = {
 		"username": os.getenv("POSTGRES_USERNAME"),
 		"password": os.getenv("POSTGRES_PASSWORD"),
 		"database": os.getenv("POSTGRES_DATABASE")
 	}
-	main(postgres_config)
-
+	main(config)
